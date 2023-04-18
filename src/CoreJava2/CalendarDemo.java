0db0a2c26ebb633e2e62fbfdf676805d93aa68c2
nandini <nndn.rwt@gmail.com>
2023-04-18 12:13:31 +0100

    this is commited by GitStuff guy which is first guy

--- src/CoreJava2/CalendarDemo.java
+++ src/CoreJava2/CalendarDemo.java
@@ -3,11 +3,18 @@ package CoreJava2;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+
+
 public class CalendarDemo {
+	
+	public void github() {
+		System.out.println("created by gitstuff master first guy");
+	}	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		CalendarDemo cd = new CalendarDemo();
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sd = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
 		System.out.println(sd.format(cal.getTime()));
@@ -22,8 +29,10 @@ public class CalendarDemo {
 		//writing someting to check gitHub functionality
 	
 		System.out.println("Code update by another person who clone the code from Repo");
-	
-	
+	   ////  now code is pulled by gitstuff again and he update the code again
+		
+		System.out.println("code updated by Gitstuff guy who is first guy");
+	    cd.github();
 	}
 
 }
